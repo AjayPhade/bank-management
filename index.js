abc2311e70b752632b11ed10f3afd7535d988cce
Ajay Phade <ajayphade@gmail.com>
2020-11-03 09:14:43 +0530

    Changed table for active loans

--- index.js
+++ index.js
@@ -580,7 +580,7 @@ app.post("/remove_customer_details", function (req, res) {
                 }
                 else {
                     if (row[0].balance > 0) {
-                        connection.query("insert into transaction (trans_type,amount,acc_no,ifsc_code,rem_bal) values(?,?,?,?,?)", ['Debit', row[0].balance, acc_no,ifsc_code,0], function (err) {
+                        connection.query("insert into transaction (trans_type,amount,acc_no,ifsc_code,rem_bal) values(?,?,?,?,?)", ['Debit', row[0].balance, acc_no, ifsc_code, 0], function (err) {
                             if (err) {
                                 console.log(err);
                             }
@@ -1040,16 +1040,16 @@ app.post("/remove_employee_details", function (req, res) {
         }
         else {
             console.log("Deleted from emp_phone");
-            connection.query("select * from employee where emp_id=?",[emp_id],function(err,row){
-                if(err){
+            connection.query("select * from employee where emp_id=?", [emp_id], function (err, row) {
+                if (err) {
                     console.log(err);
                 }
-                else if(row[0].designation === "Cashier"){
-                    connection.query("delete from cash_counter where emp_id=?",[emp_id],function(err){
-                        if(err){
+                else if (row[0].designation === "Cashier") {
+                    connection.query("delete from cash_counter where emp_id=?", [emp_id], function (err) {
+                        if (err) {
                             console.log(err);
                         }
-                        else{
+                        else {
                             connection.query("DELETE FROM employee WHERE emp_id = ?", [emp_id], function (err) {
                                 if (err) {
                                     console.log(err);
@@ -1057,14 +1057,14 @@ app.post("/remove_employee_details", function (req, res) {
                                 else {
                                     console.log("Deleted from employee");
                                     res.render("employee_mg", { ifsc_code: ifsc_code, br_name: br_name, emp_details, row: undefined, rows: undefined, error: ["success_removed", undefined] });
-                
+
                                 }
                             });
                         }
                     });
-                    
+
                 }
-                else{
+                else {
                     connection.query("DELETE FROM employee WHERE emp_id = ?", [emp_id], function (err) {
                         if (err) {
                             console.log(err);
@@ -1072,12 +1072,12 @@ app.post("/remove_employee_details", function (req, res) {
                         else {
                             console.log("Deleted from employee");
                             res.render("employee_mg", { ifsc_code: ifsc_code, br_name: br_name, emp_details, row: undefined, rows: undefined, error: ["success_removed", undefined] });
-        
+
                         }
                     });
                 }
             });
-            
+
         }
     });
 });
@@ -1116,7 +1116,7 @@ app.post("/withdraw", function (req, res) {
                 else {
                     var q = "insert into transaction (counter_no, trans_type,amount, acc_no, ifsc_code, rem_bal) values(?,?,?,?,?,?)";
 
-                    connection.query(q, [counter_no, "Debit", amount, acc_no, ifsc_code, rows[0].balance-amount], function (err) {
+                    connection.query(q, [counter_no, "Debit", amount, acc_no, ifsc_code, rows[0].balance - amount], function (err) {
                         if (err) {
                             console.log(err);
                         }
@@ -1156,7 +1156,7 @@ app.post("/deposit", function (req, res) {
             else {
                 var q = "insert into transaction (counter_no, trans_type,amount, acc_no,ifsc_code,rem_bal) values(?,?,?,?,?,?)";
 
-                connection.query(q, [counter_no, "Credit", amount, acc_no, ifsc_code, rows[0].balance+amount], function (err, rows, fields) {
+                connection.query(q, [counter_no, "Credit", amount, acc_no, ifsc_code, rows[0].balance + amount], function (err, rows, fields) {
                     if (err) {
                         console.log(err);
                     }
@@ -1542,9 +1542,26 @@ app.get("/info/critical_accounts", function (req, res) {
 
 app.get("/info/active_loans", function (req, res) {
     if (loggedIn(res)) {
-        connection.query("select *,l.int_rate from loan l join cust_info c on l.acc_no = c.acc_no where ifsc_code = ?", [ifsc_code], function (err, rows, fields) {
+        var cols = "date_format(time_stamp, '%d-%b-%Y %r') Time" +
+            ", l.acc_no 'Account No'" +
+            ", name Name" +
+            ", loan_type 'Loan'" +
+            ", amount Amount" +
+            ", rem_amt 'Remaining'" +
+            ", mortgage Mortgage" +
+            ", l.int_rate 'Interest'" +
+            ", tenure Tenure" +
+            ", sanctioned_by 'Sanctioned'" +
+            ", email Email" +
+            ", phone_no 'Phone'";
+
+        connection.query("select " + cols + " from loan l join cust_info c on l.acc_no = c.acc_no where ifsc_code = ?", [ifsc_code], function (err, rows, fields) {
             var columns = [6, 5, 12, 0, 1, 7, 2, 30, 4, 8, 13, 29];
             //console.log(fields);
+            //console.log(rows);
+
+            if(err)
+                console.log(err);
 
             res.render("info", { ifsc_code: ifsc_code, br_name: br_name, rows, columns, fields });
         });
@@ -1576,7 +1593,7 @@ app.get("/info/overdue_loans", function (req, res) {
 app.get("/info/all_transactions", function (req, res) {
     if (loggedIn(res)) {
         connection.query("select * from transaction t natural join cash_counter natural join employee where ifsc_code = ?", [ifsc_code], function (err, rows, fields) {
-            var columns = [3, 7, 4, 5, 8, 6, 2, 1,9];
+            var columns = [3, 7, 4, 5, 8, 6, 2, 1, 9];
             //console.log(fields);
 
             res.render("info", { ifsc_code: ifsc_code, br_name: br_name, rows, columns, fields });
