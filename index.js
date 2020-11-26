89f3fdcf21c19199d8ad4efaebf3b99981e7e566
Jay <jaygandhi129@gmail.com>
2020-11-26 18:06:35 +0530

    Added port

--- index.js
+++ index.js
@@ -1714,7 +1714,7 @@ app.get("/emp_details", function (req, res) {
     }
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started at port 3000");
     //console.log(storageRef);
 
